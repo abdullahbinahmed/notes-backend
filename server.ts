04dcb625ed1264bbe16d290d577284fb35392d0a
Syed Muhammad Abdullah Bin Ahmed <103053235+abdullahbinahmed@users.noreply.github.com>
2026-07-14 22:47:14 +0500

    Revert "Feat/notes application"

--- server.ts
+++
@@ -1,7 +0,0 @@
-import app from './src/app';
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server started and processing live on: http://localhost:${PORT}`);
-});
